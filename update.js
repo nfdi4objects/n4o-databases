c6940ded5b4b15bc5fbaa18e613c00ffa385390f
Jakob Voss <voss@gbv.de>
2024-03-01 09:47:22 +0100

    Add RDF/Turtle

--- update.js
+++ update.js
@@ -4,6 +4,7 @@ import fs from 'fs'
 import jsonld from 'jsonld'
 import wdk from 'wikibase-sdk/wikidata.org'
 import { parse } from 'csv-parse/sync'
+import { TurtleSerializer } from '@rdfjs-elements/formats-pretty'
 
 const wdquery = async query =>
     fetch(wdk.getEntities(query)).then(res => res.json()).then(res => Object.values(res.entities))
@@ -59,6 +60,31 @@ for (let item of items) {
   item.type = ['dcat:Catalog','nfdicore:DataPortal','fabio:Database']
 }
 const nquads = await jsonld.toRDF(items, {format: 'application/n-quads'})
-fs.writeFileSync('n4o-databases.nt',nquads)
 
 console.log(`Angaben zu ${items.length} Datenbanken aktualisiert (${nquads.split("\n").length-1} RDF triple).`)
+
+fs.writeFileSync('n4o-databases.nt',nquads)
+
+// serialize turtle
+import ParserN3 from '@rdfjs/parser-n3'
+import { Readable } from 'readable-stream'
+import getStream from 'get-stream'
+
+const prefixes = {
+  "dcat": "http://www.w3.org/ns/dcat#",
+  "dcterms": "http://purl.org/dc/terms/",
+  "fabio": "http://purl.org/spar/fabio/",
+  "foaf": "http://xmlns.com/foaf/0.1/",
+  "nfdicore": "https://nfdi.fiz-karlsruhe.de/ontology/",
+  "owl": "http://www.w3.org/2002/07/owl#",
+  "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+  "schema": "http://schema.org/",
+  "skos": "http://www.w3.org/2004/02/skos/core#",
+  "xsd": "http://www.w3.org/2001/XMLSchema#",
+  "wd": "http://www.wikidata.org/entity/",
+}
+
+const sink = new TurtleSerializer({ prefixes })
+const parserN3 = new ParserN3()
+const ttl = await getStream(sink.import(parserN3.import(Readable.from(nquads))))
+fs.writeFileSync('n4o-databases.ttl',ttl)
